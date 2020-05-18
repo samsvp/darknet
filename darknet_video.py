df18545affd914e7258854e0024c9822b5da7cda
samsvp <50253175+samsvp@users.noreply.github.com>
2020-05-18 13:07:25 -0300

    Edited Dakent Video to work with ROS

--- darknet_video.py
+++ darknet_video.py
@@ -1,13 +1,37 @@
+#!/usr/bin/env python
+
+from __future__ import print_function, division
+
+import os
+
+# change location to scripts. Remove when ros parameters are implemented
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+import rospy
+from robosub_msgs.msg import Detection, DetectionArray
+from sensor_msgs.msg import Image
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+
 from ctypes import *
 import math
 import random
-import os
-import cv2
-import numpy as np
+# import os
 import time
 import darknet
 
-def convertBack(x, y, w, h):
+
+_netMain = None
+_metaMain = None
+_darknet_image = None
+_resized_frame = None
+
+def convert_back(x, y, w, h):
+    '''
+    Converts the parameters of the boxes to the opencv format
+    '''
     xmin = int(round(x - (w / 2)))
     xmax = int(round(x + (w / 2)))
     ymin = int(round(y - (h / 2)))
@@ -15,13 +39,17 @@ def convertBack(x, y, w, h):
     return xmin, ymin, xmax, ymax
 
 
-def cvDrawBoxes(detections, img):
+def cv_draw_boxes(detections, img):
+    '''
+    Draws the detected boxes
+    '''
+    
     for detection in detections:
         x, y, w, h = detection[2][0],\
             detection[2][1],\
             detection[2][2],\
             detection[2][3]
-        xmin, ymin, xmax, ymax = convertBack(
+        xmin, ymin, xmax, ymax = convert_back(
             float(x), float(y), float(w), float(h))
         pt1 = (xmin, ymin)
         pt2 = (xmax, ymax)
@@ -34,17 +62,15 @@ def cvDrawBoxes(detections, img):
     return img
 
 
-netMain = None
-metaMain = None
-altNames = None
-
-
-def YOLO():
-
-    global metaMain, netMain, altNames
-    configPath = "./cfg/yolov4.cfg"
-    weightPath = "./yolov4.weights"
+def load_network():
+    '''
+    Initializes the darknet
+    '''
+    # use ros param for it
+    configPath = "./cfg/yolov3-tiny.cfg"
+    weightPath = "./yolov3-tiny.weights"
     metaPath = "./cfg/coco.data"
+
     if not os.path.exists(configPath):
         raise ValueError("Invalid config path `" +
                          os.path.abspath(configPath)+"`")
@@ -54,62 +80,109 @@ def YOLO():
     if not os.path.exists(metaPath):
         raise ValueError("Invalid data file path `" +
                          os.path.abspath(metaPath)+"`")
-    if netMain is None:
-        netMain = darknet.load_net_custom(configPath.encode(
-            "ascii"), weightPath.encode("ascii"), 0, 1)  # batch size = 1
-    if metaMain is None:
-        metaMain = darknet.load_meta(metaPath.encode("ascii"))
-    if altNames is None:
-        try:
-            with open(metaPath) as metaFH:
-                metaContents = metaFH.read()
-                import re
-                match = re.search("names *= *(.*)$", metaContents,
-                                  re.IGNORECASE | re.MULTILINE)
-                if match:
-                    result = match.group(1)
-                else:
-                    result = None
-                try:
-                    if os.path.exists(result):
-                        with open(result) as namesFH:
-                            namesList = namesFH.read().strip().split("\n")
-                            altNames = [x.strip() for x in namesList]
-                except TypeError:
-                    pass
-        except Exception:
-            pass
-    #cap = cv2.VideoCapture(0)
-    cap = cv2.VideoCapture("test.mp4")
-    cap.set(3, 1280)
-    cap.set(4, 720)
-    out = cv2.VideoWriter(
-        "output.avi", cv2.VideoWriter_fourcc(*"MJPG"), 10.0,
-        (darknet.network_width(netMain), darknet.network_height(netMain)))
-    print("Starting the YOLO loop...")
+    
+    netMain = darknet.load_net_custom(configPath.encode(
+        "ascii"), weightPath.encode("ascii"), 0, 1)  # batch size = 1
+
+    metaMain = darknet.load_meta(metaPath.encode("ascii"))
 
     # Create an image we reuse for each detect
     darknet_image = darknet.make_image(darknet.network_width(netMain),
                                     darknet.network_height(netMain),3)
-    while True:
+
+    return netMain, metaMain, darknet_image
+
+
+def resize_frame(cap):
+    '''
+    Resizes the images to darknet's cfg size 
+    '''
+    ret, frame_read = cap.read()
+    frame_rgb = cv2.cvtColor(frame_read, cv2.COLOR_BGR2RGB)
+    frame_resized = cv2.resize(frame_rgb,
+                                (darknet.network_width(_netMain),
+                                darknet.network_height(_netMain)),
+                                interpolation=cv2.INTER_LINEAR)
+
+    return frame_resized
+
+
+def detect():
+    '''
+    Returns the detections made by darknet and a ros detection message type
+    '''
+
+    darknet.copy_image_from_bytes(_darknet_image, _resized_frame.tobytes())
+
+    detections = darknet.detect_image(_netMain, _metaMain, _darknet_image, thresh=0.25) 
+
+    detection_array = DetectionArray() 
+
+    for d in detections:
+        box = Detection()
+
+        box.label = d[0].decode()
+
+        box.x = d[2][0] / darknet.network_width(_netMain) 
+        box.y = d[2][1] / darknet.network_height(_netMain) 
+        box.w = d[2][2] / darknet.network_width(_netMain)
+        box.h = d[2][3] / darknet.network_height(_netMain)
+
+        detection_array.boxes.append(box)
+        
+    return detections, detection_array
+    
+
+def detections_img(cap, detections):
+    '''
+    Draws the boxes on the camera image  
+    '''
+    image = cv_draw_boxes(detections, _resized_frame)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    return image
+
+
+
+def YOLO():
+    '''
+    Main YOLO loop 
+    '''
+
+    global _netMain, _metaMain, _darknet_image, _resized_frame
+
+    _netMain, _metaMain, _darknet_image = load_network()
+
+    detections_pub = rospy.Publisher('darknet', DetectionArray, queue_size=10)
+    image_pub = rospy.Publisher("darknet_image", Image, queue_size=10)
+
+    rospy.init_node('DarknetNode', anonymous=True)
+
+    cap = cv2.VideoCapture(0)
+
+    cap.set(3, 640)
+    cap.set(4, 480)
+    
+    bridge = CvBridge()
+
+    print("Starting the YOLO loop...")
+
+    while not rospy.is_shutdown():
         prev_time = time.time()
-        ret, frame_read = cap.read()
-        frame_rgb = cv2.cvtColor(frame_read, cv2.COLOR_BGR2RGB)
-        frame_resized = cv2.resize(frame_rgb,
-                                   (darknet.network_width(netMain),
-                                    darknet.network_height(netMain)),
-                                   interpolation=cv2.INTER_LINEAR)
 
-        darknet.copy_image_from_bytes(darknet_image,frame_resized.tobytes())
+        _resized_frame = resize_frame(cap)
 
-        detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.25)
-        image = cvDrawBoxes(detections, frame_resized)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        detections, detection_array = detect()
+        detections_pub.publish(detection_array)
+
+        image = detections_img(cap, detections)
+
+        image_pub.publish(bridge.cv2_to_imgmsg(image, "bgr8"))
+
         print(1/(time.time()-prev_time))
-        cv2.imshow('Demo', image)
-        cv2.waitKey(3)
+    
     cap.release()
-    out.release()
+    
 
 if __name__ == "__main__":
     YOLO()
